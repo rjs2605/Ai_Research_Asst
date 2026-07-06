a14f55359921cce14f63aeb29174375bd674e476
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:29:41 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,5 @@
 from functools import lru_cache
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -15,7 +13,6 @@ class Settings(BaseSettings):
     CHROMA_PATH: str
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-
 
 @lru_cache
 def get_settings() -> Settings:
