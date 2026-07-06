3817e82fb30169c681c46ff8be5f4764d56d03ff
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:29:03 +0530

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -8,22 +8,14 @@ router = APIRouter()
 
 rag = RAGService()
 
-
 @router.post("/ask", response_model=QuestionResponse)
 async def ask(request: QuestionRequest):
-
     answer = rag.ask(request.question)
-
     return QuestionResponse(answer=answer)
-
-
-
 
 @router.delete("/reset-db")
 async def reset():
-
     VectorStore().reset()
-
     return {
         "message": "Vector database deleted."
     }
