31c4357c268b40c416b55f7ac194bb0d148506bf
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:30:12 +0530

    Update logging.py

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,26 +1,19 @@
 import logging
 import sys
 
-
 def setup_logger(name: str = "AI_RAG_Assistant") -> logging.Logger:
     logger = logging.getLogger(name)
-
     if logger.hasHandlers():
         return logger
-
     logger.setLevel(logging.INFO)
-
     formatter = logging.Formatter(
         fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
-
     return logger
-
 
 logger = setup_logger()
